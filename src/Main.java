ca1311587d1becb7723711c0ce7244e33d4350ce
Ryan Bachman <jacksprat455@gmail.com>
2018-07-24 10:38:18 -0500

    Values

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        int number1 = 0;
-        int number2 = 0;
+        int number1 = 3;
+        int number2 = 5;
 
-        System.out.println(“The largest number is: “ + largestNumber(number1, number2))
+        System.out.println("The largest number is:" + sumNum(number1, number2));
     }
 
-    public static int 
+    public static int sumNum( int n1, int n2){
+        if (n2 > n1){
+            return n2;
+        }
+        else{
+            return n1;
+        }
+    }
 }
